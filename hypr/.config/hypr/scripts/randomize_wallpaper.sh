fd1abcca048d10977c874cac8fed5cf81459d94f
Sam Burcksen <sam.burcksen@gmail.com>
2024-11-23 03:00:40 +0100

    Adjust randomize wallpaper behavior

--- hypr/.config/hypr/scripts/randomize_wallpaper.sh
+++ hypr/.config/hypr/scripts/randomize_wallpaper.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Selects a random image from the Wallpaper folder
-# And tells hyprpaper to set it as current wallpaper
+# and tells hyprpaper to set it as current wallpaper
 
 wallpaper_folder=~/Images/Wallpapers
 wallpaper=$(find $wallpaper_folder -maxdepth 1 -type f | shuf -n 1)
 
+hyprctl hyprpaper unload all
 hyprctl hyprpaper preload "$wallpaper"
 hyprctl hyprpaper wallpaper ", $wallpaper"
