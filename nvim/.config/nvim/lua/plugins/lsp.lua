ed3bfcacd42340a4e3f072c63f7e5b700c99864c
Sam Burcksen <sam.burcksen@gmail.com>
2025-01-05 15:28:28 +0100

    Fix autocomplete not working properly

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -67,6 +67,12 @@ return {
         -- Autocompletion
         local cmp = require('cmp')
         cmp.setup({
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "path" },
+                { name = "buffer" },
+            },
+
             mapping = cmp.mapping.preset.insert({
                 ["<C-Space>"] = cmp.mapping.complete(),
                 --['<CR>'] = cmp.mapping.confirm({ select = true }),
