c83345fa5a1807a71125a7591eae06d912b99d98
Sam Burcksen <sam.burcksen@gmail.com>
2025-01-14 21:20:28 +0100

    Move gitconfig to home-manager

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -1,35 +1,25 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./nvim.nix
     ./fonts.nix
     ./fish.nix
+    ./git.nix
   ];
 
   home.username = "sburcksen";
   home.homeDirectory = "/home/sburcksen";
 
-  home.stateVersion = "24.11";
-
-  home.packages = [
+  home.packages = with pkgs; [
+    firefox
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home.stateVersion = "24.11";
 }
