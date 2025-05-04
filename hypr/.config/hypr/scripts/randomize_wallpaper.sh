d1a634169a6ab52c19e248ee38c337e205bd878f
Sam Burcksen <sam.burcksen@gmail.com>
2025-05-04 18:02:06 +0200

    Add Discord

--- hypr/.config/hypr/scripts/randomize_wallpaper.sh
+++ hypr/.config/hypr/scripts/randomize_wallpaper.sh
@@ -3,7 +3,7 @@
 # Selects a random image from the Wallpaper folder
 # and tells hyprpaper to set it as current wallpaper
 
-wallpaper_folder=~/Images/Wallpapers
+wallpaper_folder=~/Images/wallpapers
 wallpaper=$(find $wallpaper_folder -maxdepth 1 -type f | shuf -n 1)
 
 hyprctl hyprpaper unload all
